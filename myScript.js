77175a509205f052b436302cc9b5464e55db568e
maiya0902 <52472592+maiya0902@users.noreply.github.com>
2019-07-03 11:01:05 -0700

    gif not GIF

--- myScript.js
+++ myScript.js
@@ -2,9 +2,9 @@ function myFunction() {
   var txt;
   if (confirm("Orcas are apex predators meaning that they are at the very top of the food chain. No other animal hunts orcas (humans excluded). Click OK to see some more orcas!")) {
     txt = "You pressed OK!";
-    document.getElementById('gifImage').src='orcigiphy1.GIF';
-    document.getElementById('gifImage2').src='orcigiphy2.GIF';
-    document.getElementById('gifImage3').src='orcigiphy3.GIF';
+    document.getElementById('gifImage').src='orcigiphy1.gif';
+    document.getElementById('gifImage2').src='orcigiphy2.gif';
+    document.getElementById('gifImage3').src='orcigiphy3.gif';
   } else {
     txt = "You pressed Cancel!";
   }
