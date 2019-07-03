b2ad03d312d71cfb60f2cbc089ba57566c3a0ad7
maiya0902 <52472592+maiya0902@users.noreply.github.com>
2019-07-03 10:27:58 -0700

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,7 +1,10 @@
 function myFunction() {
   var txt;
-  if (confirm("Press a button!")) {
+  if (confirm("Orcas are apex predators meaning that they are at the very top of the food chain. No other animal hunts orcas (humans excluded).")) {
     txt = "You pressed OK!";
+    document.getElementById('gifImage').src='orcigiphy1.GIF';
+    document.getElementById('gifImage2').src='orcigiphy2.GIF';
+    document.getElementById('gifImage3').src='orcigiphy3.GIF';
   } else {
     txt = "You pressed Cancel!";
   }
